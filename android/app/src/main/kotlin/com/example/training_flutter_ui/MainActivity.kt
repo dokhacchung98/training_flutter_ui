4f709a614707dfdae5011c985b339546627075c6
dokhacchung98 <ochung3@gmail.com>
2022-05-10 10:44:33 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/training_flutter_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.training_flutter_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/color_constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static const Color colorMain1 = Color(0xFF2B5876);
+  static const Color colorMain2 = Color(0xFF4E4376);
+  static const Color colorWhite100 = Color(0xFFFFFFFF);
+  static const Color colorWhite75 = Color(0xBF4E4376);
+  static const Color colorWhite50 = Color(0x804E4376);
+  static const Color colorYellow = Color(0xFFF5C518);
+  static const Color colorBlack = Color(0xFF000000);
+  static const Color colorRectangle1 = Color(0xFFA6A1E0);
+  static const Color colorRectangle2 = Color(0xFFA1F3FE);
+  static const Color colorPaging1 = Color(0xFF64ABDB);
+  static const Color colorPaging2 = Color(0xFF826EC8);
+}
